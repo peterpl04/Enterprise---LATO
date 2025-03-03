f1ff36bda08a0b781519405db59b7d6011cb0ae8
Pedro de Lara <148505695+peterpl04@users.noreply.github.com>
2025-03-03 15:35:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function servicos() {
+  alert('Serviços');
+}
+function maquinas() {
+  alert('Máquinas');
+}
+function contato() {
+  alert('Contato');
+}
+function sobre() {
+  alert('Sobre');
+}
